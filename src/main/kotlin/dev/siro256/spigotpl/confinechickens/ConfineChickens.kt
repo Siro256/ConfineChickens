f1dd0c69fff8202dbcc1f0443a055834c7cefc83
Siro_256 <siro@siro256.dev>
2021-11-20 20:53:56 +0900

    Load team in startup

--- src/main/kotlin/dev/siro256/spigotpl/confinechickens/ConfineChickens.kt
+++ src/main/kotlin/dev/siro256/spigotpl/confinechickens/ConfineChickens.kt
@@ -6,6 +6,14 @@ import org.bukkit.scoreboard.Team
 
 class ConfineChickens: JavaPlugin() {
     override fun onEnable() {
+        nonCollisionTeam = if (server.scoreboardManager!!.mainScoreboard.getTeam("nonCollisionTeam") != null) {
+            server.scoreboardManager!!.mainScoreboard.getTeam("nonCollisionTeam")!!
+        } else {
+            server.scoreboardManager!!.mainScoreboard.registerNewTeam("nonCollisionTeam").apply {
+                setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER)
+            }
+        }
+
         server.consoleSender.sendMessage("[ConfineChickens] ${ChatColor.GREEN}ConfineChickens enabled.")
     }
 
